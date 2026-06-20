ba2852e0e76277de527ba3f3c5339f2cf8cd8916
Nagubandi srikar <nagubandisrikar73@gmail.com>
2026-06-20 16:03:09 +0530

    Update and rename app.py to loop.py
    and also i learned of inside and outside in coding

---
+++ loop.py
@@ -0,0 +1,17 @@
+i=1
+while i<=10 :
+    if i % 2 == 1 :
+        print(i)
+    i=i+1
+
+for i in range(1, 10):
+    for k in range(i):
+            print("*", end=" ")
+    print()
+
+
+for i in range(1, 4):
+    print(i)          
+    print("hello")    
+    
+print('done')         
